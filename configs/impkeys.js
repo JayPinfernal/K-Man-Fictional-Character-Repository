fce82d2da14f9ef0110bfca2ab77372229634e7e
Unknown <jellalfan21@gmail.com>
2017-10-24 12:53:59 +0530

    This is a test to check if ckedit works on heroku

---
+++ configs/impkeys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI :'',
+  googleClientID: '11364369271-lpr2i57pu8ngb55uvg5pk5gihntpg1df.apps.googleusercontent.com'
+  googleClientSecret: 'S_om7alRPO3NEqIo6_89q0SU'
+};
--- start.js
+++ start.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const mongoose= require('mongoose');
+const exphbs = require('express-handlebars');
+const bodyParser =require('body-parser');
 
 const app = express();
+//handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//some demo code for now
+app.get('/demo', (req, res) => {
+  res.render('myDemo');
+});
+
+app.post('/doStuff', (req, res) => {
+  //console.log(req.body.hide);
+   console.log('this is a response');
+   console.log(req.body);
+  res.render('myview',{
+    info:req.body.datval
+  });
+});
 
 const port =process.env.PORT || 5523;
 
